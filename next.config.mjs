70499885521c39b8bc8200d13f15c85942187bbe
inbeom <dlsqjatm@naver.com>
2024-05-11 10:37:49 +0900

    config cors

--- next.config.mjs
+++ next.config.mjs
@@ -4,14 +4,27 @@ const nextConfig = {
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/message", // 요청한 경로
-  //       destination: `https://blog-egdo4ft4v-beomis-projects.vercel.app/guestbook`, // 내가 사용할 경로
-  //     },
-  //   ];
-  // },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
   async redirects() {
     return [
       {
